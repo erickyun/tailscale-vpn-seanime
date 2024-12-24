b14fc568a905d64705922ca35c674e3de2645cc3
erickyun <71968711+erickyun@users.noreply.github.com>
2024-12-24 15:21:43 +0300

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -3,4 +3,3 @@
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
 echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/

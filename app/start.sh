0217be4a9798c4db4868c015f3f7988c44dc0705
erickyun <71968711+erickyun@users.noreply.github.com>
2024-12-26 08:15:14 +0300

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -4,4 +4,4 @@
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
 echo Tailscale started
 ALL_PROXY=socks5://localhost:1055/
-gunicorn --bind 0.0.0.0:${PORT} --workers 2 --threads 2 app.wsgi
+gunicorn --bind 0.0.0.0:${PORT} --workers 2 --threads 2 app.wsgi:app

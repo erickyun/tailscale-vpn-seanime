5388194d4cdc0e996b6eae3da06a47c57732a286
erickyun <71968711+erickyun@users.noreply.github.com>
2024-12-26 08:20:06 +0300

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+curl -o /app/config/Seanime/seanime.db -L $ACCS
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
 echo Tailscale started

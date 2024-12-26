95b9213b8183b3d0a6a1c50abed54a013f82d401
erickyun <71968711+erickyun@users.noreply.github.com>
2024-12-26 08:51:59 +0300

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 curl -o /app/config/Seanime/seanime.db -L $ACCS
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node --accept-dns=false
-echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/
-#gunicorn --bind 0.0.0.0:${PORT} --workers 2 --threads 2 app.wsgi:app
+#/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+#/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
+#echo Tailscale started
+wget -O /app/wireguard.sh https://get.vpnsetup.net/wg
+chmod +x /app/wireguard.sh
+sudo bash /app/wireguard.sh --auto
